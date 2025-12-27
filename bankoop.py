c2726fe3dc4c0d089fe80614e97cb91b33bbf290
theHrDev <hamzatrukayat2822@gmail.com>
2025-12-27 17:38:13 +0100

    Added the authenticated user action class

--- bankoop.py
+++ bankoop.py
@@ -3,14 +3,14 @@ import random
 class User:
     def __init__(self):
         self.users = {}
-        
+        self.autheticated_user = {}
     def create_account(self,firstname,lastname,email,password):
         if email in self.users:
             return "User with the email already exist"
         account_number = random.randint(10**9,10**10-1)
-        new_user = {"firstname":firstname,"lastname":lastname,"email":email,"password":password,"account_number":account_number}
+        new_user = {"firstname":firstname,"lastname":lastname,"email":email,"password":password,"account_number":account_number,"account_balance":50000}
         self.users[email] = new_user
-        return "Account successfully created"
+        return f"Account successfully created, your account balance is {new_user['account_number']} and your account has been credited with $50000 bonus"
     
     def view_single_user(self,email):
         if not self.users[email]:
@@ -18,7 +18,17 @@ class User:
         return self.users[email]
     
     def login(self,email,password):
-        pass
+        if email not in self.users:
+            return "Invalid credentials"
+        else:
+            intended_user_details = self.users[email]
+            if password == intended_user_details[password] and email == intended_user_details[email]:
+                self.autheticated_user = self.users[email]
+                
+                return "Login Successfully"
+            else:
+                return "Invalid Credentials"
+        
         
     def view_users(self):
         if not self.users:
@@ -26,9 +36,35 @@ class User:
         return self.users
         
         
-class Authenticated_Actions:
+class Authenticated_Actions(User):
     def __init__(self):
-        pass
+        super().__init__()
+        
+    def transfer(self,recepient_acc_num,amount_to_send,password):
+        if amount_to_send > self.autheticated_user["account_balance"]:
+            return "Insufficient Fund"
+        else:
+            if password == self.autheticated_user["password"]:
+                self.autheticated_user["account_balance"] = self.autheticated_user["account_balance"] - amount_to_send
+                return f"{amount_to_send} successfully sent to {recepient_acc_num}"
+            
+            else:
+                return "Incorrect password"
+            
+    def check_balance(self):
+        return f"Your account balance is {self.autheticated_user["account_balance"]}"
+    
+    def edit_info(self,key,value):
+        if key not in self.autheticated_user:
+            return f"No key with the input key, {key}"
+        else:
+            self.autheticated_user[key] = value
+            return f"Edited successfully, your details is now {self.autheticated_user}"
+        
+    def logout(self):
+        self.autheticated_user = {}
+        return "Logout successfully"
+   
     
 
 def bank_app():
@@ -54,6 +90,23 @@ def bank_app():
             email = input("Enter your email: ")
             password = input("Enter your password: ")
             print(user.login(email=email,password=password))
+            auth_user_action = Authenticated_Actions()
+            while True:
+                print("------- 1. Make Transfer ---------")
+                print("------- 2. Check Balance ---------")
+                print("------- 3. Edit Info ---------")
+                print("------- 4. Logout ---------")
+                
+                auth_menu = input("Enter the action you want to perform: ")
+                if auth_menu == "1":
+                    receipient = input("Enter the receipient account number: ")
+                    amount = input("Enter the amount you want to send: ")
+                    password = input("Enter your password: ")
+                    print(auth_user_action.transfer(recepient_acc_num=receipient,amount_to_send=amount,password=password))
+                    
+                elif auth_menu == "2":
+                    print(auth_user_action.check_balance())
+                
             
         elif choice == "3":
             email = input("Enter your email: ")
